2378ede9b0f3ce53186d4eb2171ccb717c8320a0
Bryan Schneller <bryan.schneller@gmail.com>
2024-02-14 12:58:08 -0500

    Add get and post routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,49 @@
 const notesRouter = require('express').Router();
+const fs = require('fs');
 
 notesRouter.get('/', (req, res) => {
-    console.log("hi guys or something")
+    fs.readFile('./db/db.json', (error, data) => {
+        if(error){
+            console.log(error);
+        } else {
+            const note = JSON.parse(data);
+            res.json(note);
+        }
+    })
 });
 
-//Daunting task of writing the logic to make the routes work
 notesRouter.post('/', (req, res) => {
-    console.log(req.body);
+    const {title, content} = req.body;
+    if(title && content){
+        const addedNote = {
+            title, 
+            content
+        }
+
+        fs.readFile('./db/db.json', (error, content) => {
+            if(error){
+                console.log(error);
+            } else {
+                const notes = JSON.parse(content);
+                notes.push(addedNote);
+                const stringifiedNotes = JSON.stringify(notes);
+                fs.writeFile('./db/db.json', stringifiedNotes, (error) => {
+                    if(error){
+                        console.log(error);
+                    }
+                })
+            }
+        })
+
+        const response = {
+            status: 'Note created successfully',
+            body: addedNote
+        }
+
+        res.status(201).json(response);
+    } else {
+        res.status(500).json("There was an error saving the new note. Please try again.")
+    }
 });
 
 module.exports = notesRouter;
